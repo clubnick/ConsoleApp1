539568d6054396a2626c535d302a5415187d037b
Stanislav Klabnik <stanislav.klabnik@outlook.com>
2019-11-23 17:42:23 +0100

    1.0.5.3 add console write version 1.0.5.3

--- Program.cs
+++ Program.cs
@@ -57,12 +57,12 @@ namespace ConsoleApp1
             using (var bcte = TCCreator.TestEngineBonusCampaign(stepParameters))
             {
 
-                Console.WriteLine("Start version:1.0.5.2");
+                Console.WriteLine("Start version:1.0.5.3");
 
                 Console.WriteLine("Bonus campaign: {0}", bcte.BonusCampaignParameters.BonusId);
                 Console.WriteLine("Bonus campaign: {0}", bcte.BonusCampaignParameters.BonusName);
 
-                Console.WriteLine("Finish}");
+                Console.WriteLine("Finish version:1.0.5.3");
             }
 
 
