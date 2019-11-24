ddcbf65c39a7b6f171a8e3925b2e252f96f79098
stanislav.klabnik <stanislav.klabnik@outlook.com>
2019-11-24 17:53:45 +0100

    1.0.11.0

--- Program.cs
+++ Program.cs
@@ -55,7 +55,7 @@ namespace ConsoleApp1
         {
             var stepParameters = TestData.BCStepParameters;
 
-            Console.WriteLine("Start version:1.0.10.0"+Environment.NewLine);
+            Console.WriteLine("Start version:1.0.11.0"+Environment.NewLine);
             using (var bcte = TCCreator.TestEngineBonusCampaign(stepParameters))
             {
                 Console.WriteLine("BCAtype: {0}", bcte.BonusCampaignParameters.BCAType);
@@ -65,7 +65,7 @@ namespace ConsoleApp1
                 Console.WriteLine("BCVCustomerCardOnly: {0}", bcte.BonusCampaignParameters.BCVCustomerCardOnly);
                 Console.WriteLine("BCSStakeRedeemable: {0}", bcte.BonusCampaignParameters.BCSStakeRedeemable);
             }
-            Console.WriteLine(Environment.NewLine+"Finish version:1.0.10.1");
+            Console.WriteLine(Environment.NewLine+"Finish version:1.0.11.0");
 
             BONUS_CAMPAIGN_ACCA_INSURANCE_DRAFT();
         }
@@ -75,6 +75,13 @@ namespace ConsoleApp1
             Console.WriteLine(Environment.NewLine);
             Console.WriteLine("Start method BONUS_CAMPAIGN_ACCA_INSURANCE_DRAFT" + Environment.NewLine);
             Console.WriteLine("Finish method BONUS_CAMPAIGN_ACCA_INSURANCE_DRAFT" + Environment.NewLine);
+        }
+
+        public static void BONUS_CAMPAIGN_FREE_BET_DRAFT()
+        {
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Start method BONUS_CAMPAIGN_FREE_BET_DRAFT" + Environment.NewLine);
+            Console.WriteLine("Finish method BONUS_CAMPAIGN_FREE_BET_DRAFT" + Environment.NewLine);
         }
     }
 }
